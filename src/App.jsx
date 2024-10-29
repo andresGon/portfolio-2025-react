cf2dc69e372c082800e3b1dc2d2f293f949e358b
andres gonzalez <andres.gonzalez@cluvi.com>
2024-10-29 09:45:52 -0500

    no context

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
 import React, { useState } from 'react';
-import { useModal, ModalProvider } from './context/modalContext.jsx'; // Importar el contexto
-import Work from "./components/Work.jsx";
-import Skills from "./components/Skills.jsx";
-import About from "./components/About.jsx";
-import Contact from "./components/Contact.jsx";
 import './index.css';
 
 function App() {
-  const [modalValue, setModalValue] = useState('');
-
-  const modalHandler = (modalName) => {
-    setModalValue(modalName);
-  };
-
-  const renderModal = () => {
-    switch (modalValue) {
-      case 'work':
-        return <Work />;
-      case 'skills':
-        return <Skills />;
-      case 'about':
-        return <About />;
-      case 'contact':
-        return <Contact />;
-      default:
-        return null;
-    }
-  };
 
   return (
     <>
-      <ModalProvider>
+      
       <div className="mainHero">
         <div className="folio">Folio 2024</div>
         <div className="lang">Esp</div>
@@ -45,20 +20,19 @@ function App() {
               <span className='char char6'>s</span>
             </span>
             <div className='last-name'>
-              Gonzalez
+              González
             </div>
           </div>
           <div className="charge">Front end / ui developer</div>
           <nav>
-            <button onClick={() => modalHandler('work')}>Work</button>
-            <button onClick={() => modalHandler('skills')}>Skills</button>
-            <button onClick={() => modalHandler('about')}>About me</button>
-            <button onClick={() => modalHandler('contact')}>Contact</button>
+            <button >Work</button>
+            <button >Skills</button>
+            <button>About me</button>
+            <button>Contact</button>
           </nav>
         </div>
       </div>
-      {renderModal()}
-      </ModalProvider>
+      
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
 function About() {
+
     return(
         <>
             <p>About</p>
+            <button>cerrar</button>
         </>
     )
 }
