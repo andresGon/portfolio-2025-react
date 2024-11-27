10d65724e9c8c3d8ffcf0b2b8581356e8ad4b96d
andres gonzalez <andres.gonzalez@cluvi.com>
2024-11-26 20:34:16 -0500

    avance textos experiencia

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,7 @@ function About() {
             <div className="title-1">{t('title_About')}</div>
             <p className="text-1">{t('description_About')}</p>
             <div className="timeLine-wrap">
-                <div className="timeLine-item">
-                    <div className="timeLine-years">2014 - 2016</div>
-                    <div className="timeLine-title">Books and books Ltda.</div>
-                    <div className="timeLine-charge">{t('about_charge_1')}</div>
-                    <div className="timeLine-text">{t('about_description_1')}</div>
-                </div>
+                
 
                 <div className="timeLine-item">
                     <div className="timeLine-years">2016 - 2021 </div>
@@ -24,16 +19,16 @@ function About() {
                 <div className="timeLine-item">
                     <div className="timeLine-years">2021 - 2022 </div>
                     <div className="timeLine-title">Titamedia</div>
-                    <div className="timeLine-charge">UI developer</div>
-                    <div className="timeLine-text">descripciotion</div>
+                    <div className="timeLine-charge">{t('about_charge_3')}</div>
+                    <div className="timeLine-text">{t('about_description_3')}</div>
                 </div>
 
 
                 <div className="timeLine-item">
                     <div className="timeLine-years">2023 - 2024 </div>
                     <div className="timeLine-title">Cluvi</div>
-                    <div className="timeLine-charge">UI developer</div>
-                    <div className="timeLine-text">descripciotion</div>
+                    <div className="timeLine-charge">{t('about_charge_4')}</div>
+                    <div className="timeLine-text">{t('about_description_4')}</div>
                 </div>
             </div>
         </>
