9839378b98a489e2e52af6e454562bc403e28562
andres gonzalez <andres.gonzalez@cluvi.com>
2024-11-30 15:03:19 -0500

    ajustes boton close

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'; // Importa el hook de traducción
+import FuegoMaiz from '../assets/fuego-y-maiz-cover.jpg'
 
 function Work() {
     const { t } = useTranslation();
@@ -6,6 +7,12 @@ function Work() {
         <>
             <div className="title-1">{t('title_work')}</div>
             <p className="text-1">{t('text_work')}</p>
+            <div className='work-wrap'>
+                <div className='work-item'>
+                    <img src={FuegoMaiz} alt="Fuego y Maíz Menú digital"/>
+                </div>
+            </div>
+            
         </>
     )
 }
